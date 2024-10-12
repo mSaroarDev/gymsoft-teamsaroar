acb1e0ed4323062be4b7429e602c165bf02eb732
mSaroarDev <msaroa.dev@gmail.com>
2024-10-12 09:56:21 +0600

    dashboard card updated

--- src/app/(pages)/dashboard/overview/DataList.jsx
+++ src/app/(pages)/dashboard/overview/DataList.jsx
@@ -32,35 +32,35 @@ const DataList = () => {
               title={"Total Users"}
               number={statistics?.totalUsers}
               percent={"+50%"}
-              icon={<UsersRound className="w-7 h-7 text-white" />}
+              icon={<UsersRound className="w-7 h-7 text-brand" />}
             />
 
             <StatsCard
               title={"Total Trainers"}
               number={statistics?.totalTrainers}
               percent={"+0%"}
-              icon={<UserCog className="w-7 h-7 text-white" />}
+              icon={<UserCog className="w-7 h-7 text-brand" />}
             />
 
             <StatsCard
               title={"Total Trainees"}
               number={statistics?.totalTrainees}
               percent={"+10%"}
-              icon={<FileUser className="w-7 h-7 text-white" />}
+              icon={<FileUser className="w-7 h-7 text-brand" />}
             />
 
             <StatsCard
               title={"Total Admins"}
               number={statistics?.totalAdmins}
               percent={"+0%"}
-              icon={<ContactRound className="w-7 h-7 text-white" />}
+              icon={<ContactRound className="w-7 h-7 text-brand" />}
             />
 
             <StatsCard
               title={"Book Requests"}
               number={statistics?.bookRequests}
               percent={"+90%"}
-              icon={<ClipboardPlus className="w-7 h-7 text-white" />}
+              icon={<ClipboardPlus className="w-7 h-7 text-brand" />}
             />
           </>
         )}
@@ -71,14 +71,14 @@ const DataList = () => {
               title={"Pending Requests"}
               number={statistics?.myPendingRequests}
               percent={"+20%"}
-              icon={<ShieldAlert className="w-7 h-7 text-white" />}
+              icon={<ShieldAlert className="w-7 h-7 text-brand" />}
             />
 
             <StatsCard
               title={"All Rquests"}
               number={statistics?.myAllRquests}
               percent={"+0%"}
-              icon={<OctagonAlert className="w-7 h-7 text-white" />}
+              icon={<OctagonAlert className="w-7 h-7 text-brand" />}
             />
           </>
         )}
@@ -89,14 +89,14 @@ const DataList = () => {
               title={"Total Schedules"}
               number={statistics?.totalSchedules}
               percent={"+75%"}
-              icon={<ShieldAlert className="w-7 h-7 text-white" />}
+              icon={<ShieldAlert className="w-7 h-7 text-brand" />}
             />
 
             <StatsCard
               title={"All AssignedRequest"}
               number={statistics?.allAssignedRequest}
               percent={"+56%"}
-              icon={<OctagonAlert className="w-7 h-7 text-white" />}
+              icon={<OctagonAlert className="w-7 h-7 text-brand" />}
             />
           </>
         )}
--- src/app/(pages)/dashboard/overview/StatsCard.jsx
+++ src/app/(pages)/dashboard/overview/StatsCard.jsx
@@ -4,7 +4,7 @@ import { Atom } from "lucide-react";
 const StatsCard = ({title, number, percent, icon}) => {
   return (
     <>
-      <div className="bg-white col-span-12 md:col-span-6 lg:col-span-3 box-shadow rounded-lg p-7">
+      <div className="bg-white col-span-12 md:col-span-6 lg:col-span-3 box-shadow rounded-lg p-7 border-s-[3px] border-brand">
         <div className="w-full flex items-start justify-between">
           <div>
             <p className="text-sm">{title}</p>
@@ -14,7 +14,7 @@ const StatsCard = ({title, number, percent, icon}) => {
             </span>{" "}
             <span>This Week</span>
           </div>
-          <div className="min-w-16 h-16 rounded-full bg-brand  flex items-center justify-center">
+          <div className="min-w-16 h-16 rounded-full bg-brand/10 flex items-center justify-center">
             {icon}
           </div>
         </div>
