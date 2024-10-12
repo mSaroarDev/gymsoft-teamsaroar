31c9f8d266235900a3cc96645bb30befd92d969d
mSaroarDev <msaroa.dev@gmail.com>
2024-10-12 17:49:41 +0600

    trainer list in trainers page is updated

--- src/app/(pages)/dashboard/(admin)/all-trainers/DataList.jsx
+++ src/app/(pages)/dashboard/(admin)/all-trainers/DataList.jsx
@@ -1,17 +1,14 @@
-'use client';
+"use client";
 import UserRow from "./UserRow";
-import { allUsers } from "@/libs/user";
 import { fetchUsers } from "@/redux/features/users/userSlice";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { useEffect } from "react";
 
 const DataList = () => {
-  // const res = await allUsers();
-  // const usersData = await res.data.data;
   const dispatch = useAppDispatch();
-  
+
   const { usersData } = useAppSelector((state) => state.users);
-  // const tainers = usersData?.filter((item)=> item?.role === "Trainer")
+  const tainers = usersData?.filter((item) => item?.role === "Trainer");
 
   useEffect(() => {
     dispatch(fetchUsers());
@@ -19,8 +16,8 @@ const DataList = () => {
 
   return (
     <>
-      {usersData &&
-        usersData?.map((item, i) => (
+      {tainers &&
+        tainers?.map((item, i) => (
           <UserRow key={item?._id} data={item} i={i} />
         ))}
     </>
